56e8f16a27ce5e4a159b36532b82c48a4bc86aaa
Andre Rodrigues <arodrigues@groupon.com>
2015-09-18 15:02:38 -0700

    Adds license information to new files.

--- osgi-bundles/libs/killbill/src/main/java/org/killbill/killbill/osgi/libs/killbill/OSGIKillbillMetrics.java
+++ osgi-bundles/libs/killbill/src/main/java/org/killbill/killbill/osgi/libs/killbill/OSGIKillbillMetrics.java
@@ -1,12 +1,25 @@
+/*
+ * Copyright 2015 Groupon, Inc
+ *
+ * Groupon licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.killbill.killbill.osgi.libs.killbill;
 
 import org.killbill.billing.osgi.api.OSGIMetrics;
 import org.osgi.framework.BundleContext;
 import org.osgi.util.tracker.ServiceTracker;
 
-/**
- * Created by arodrigues on 9/17/15.
- */
 public class OSGIKillbillMetrics extends OSGIKillbillLibraryBase implements OSGIMetrics {
 
     private final ServiceTracker killbillTracker;
--- osgi/src/main/java/org/killbill/billing/osgi/OSGIMetricsFactory.java
+++ osgi/src/main/java/org/killbill/billing/osgi/OSGIMetricsFactory.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Groupon, Inc
+ *
+ * Groupon licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.killbill.billing.osgi;
 
 import com.codahale.metrics.MetricRegistry;
@@ -8,9 +24,6 @@ import org.osgi.framework.ServiceRegistration;
 
 import javax.inject.Inject;
 
-/**
- * Created by arodrigues on 9/17/15.
- */
 public class OSGIMetricsFactory implements ServiceFactory<OSGIMetrics> {
 
     private final MetricRegistry metricRegistry;
