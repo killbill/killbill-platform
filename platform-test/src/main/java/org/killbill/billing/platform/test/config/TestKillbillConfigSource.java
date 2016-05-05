cc08e49e789bbc5014d8a4268c5eb8d8208022dc
stephane brossier <sbrossier@groupon.com>
2016-05-05 10:00:03 -0700

    test config: Revert to previous default behavior (queue mode and main notification sleep time can be configured through the use of system properties)

--- platform-test/src/main/java/org/killbill/billing/platform/test/config/TestKillbillConfigSource.java
+++ platform-test/src/main/java/org/killbill/billing/platform/test/config/TestKillbillConfigSource.java
@@ -84,7 +84,7 @@ public class TestKillbillConfigSource extends DefaultKillbillConfigSource {
         }
 
         // Speed up the notification queue
-        properties.put("org.killbill.notificationq.main.sleep", "300");
+        properties.put("org.killbill.notificationq.main.sleep", "100");
         properties.put("org.killbill.notificationq.main.nbThreads", "1");
         properties.put("org.killbill.notificationq.main.claimed", "1");
         properties.put("org.killbill.notificationq.main.queue.mode", "STICKY_POLLING");
@@ -93,12 +93,12 @@ public class TestKillbillConfigSource extends DefaultKillbillConfigSource {
         properties.put("org.killbill.persistent.bus.main.sleep", "100");
         properties.put("org.killbill.persistent.bus.main.nbThreads", "1");
         properties.put("org.killbill.persistent.bus.main.claimed", "1");
-        properties.put("org.killbill.persistent.bus.main.queue.mode", "STICKY_EVENTS");
+        properties.put("org.killbill.persistent.bus.main.queue.mode", "STICKY_POLLING");
 
         properties.put("org.killbill.persistent.bus.external.sleep", "100");
         properties.put("org.killbill.persistent.bus.external.nbThreads", "1");
         properties.put("org.killbill.persistent.bus.external.claimed", "1");
-        properties.put("org.killbill.persistent.bus.external.queue.mode", "STICKY_EVENTS");
+        properties.put("org.killbill.persistent.bus.external.queue.mode", "STICKY_POLLING");
 
         // Temporary directory for OSGI bundles
         properties.put("org.killbill.osgi.root.dir", Files.createTempDir().getAbsolutePath());
