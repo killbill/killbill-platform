8bc082f34a5e81adfbab1c0e1c6760174629f87d
Pierre-Alexandre Meyer <pierre@mouraf.org>
2018-01-30 20:55:55 -0800

    osgi: change org.killbill.billing.osgi.dao.leakDetectionThreshold to TimeSpan
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- osgi/src/main/java/org/killbill/billing/osgi/glue/OSGIDataSourceConfig.java
+++ osgi/src/main/java/org/killbill/billing/osgi/glue/OSGIDataSourceConfig.java
@@ -63,8 +63,8 @@ public interface OSGIDataSourceConfig extends DaoConfig {
 
     @Description("Amount of time that a connection can be out of the pool before a message is logged indicating a possible connection leak")
     @Config(DATA_SOURCE_PROP_PREFIX + "leakDetectionThreshold")
-    @Default("60")
-    long getLeakDetectionThreshold();
+    @Default("60s")
+    TimeSpan getLeakDetectionThreshold();
 
     @Override
     @Description("How long to wait before a connection attempt to the database is considered timed out")
